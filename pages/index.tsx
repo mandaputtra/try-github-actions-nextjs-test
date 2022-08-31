258246d537b31e5823ffea634183bf74f7d9071c
Manda Putra <mandaputra8@gmail.com>
2022-08-31 19:56:01 +0700

    feat: nambah fitur

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,11 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
